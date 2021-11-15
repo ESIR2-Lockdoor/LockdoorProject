1200386e78a79380a38a60447393246f2fcd7f66
TozzCox <78095778+TozzCox@users.noreply.github.com>
2021-11-15 16:21:42 +0100

    first commit - base react

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,50 +1,16 @@
-import React from 'react';
+import Home from "./pages/Home";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import MyProfil from "./pages/MyProfil";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom';
-
-import Team from './pages/Team/Team';
-import Contact from './pages/Contact/Contact';
-import Project from './pages/Project/Project';
-import Fonctionnement from './pages/Fonctionnement/Fonctionnement';
-import Dashboard from './pages/Dashboard/Dashboard';
-import LockDoor from './pages/LockDoor/LockDoor';
-import Navbar from './components/Navbar/Navbar';
-
-import "./App.css"
-
-const App = () => {
+function App() {
   return (
-   <Router>
-    <Navbar/>
-    <main>
-      <Switch>
-        <Route path="/" exact>
-          <Project/>
-        </Route>
-        <Route path="/team" exact>
-          <Team/>
-        </Route>
-        <Route path="/functioning" exact>
-          <Fonctionnement/>
-        </Route>
-        <Route path="/dashboard" exact>
-          <Dashboard/>
-        </Route>
-        <Route path="/contact" exact>
-          <Contact/>
-        </Route>
-        <Route path="/lockdoor" exact>
-          <LockDoor/>
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </main>
-   </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path="/myProfil" element={<MyProfil />}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ Website/src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <NavLink exact to="/" activeClassName="nav-active">
+                Accueil
+            </NavLink>
+            <NavLink exact to="/myProfil" activeClassName="nav-active">
+                Mon profil
+            </NavLink>
+        </div>
+    );
+};
+
+export default Navigation;
